562c705e4c11aee28a29def0df0785ca414ea93a
Elezthem <97851451+Elezthem@users.noreply.github.com>
2023-07-15 22:15:57 +0300

    Add files via upload

---
+++ CSLight/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sectors = { 10, 27, 19, 20, 33 };
+            bool isOpen = true;
+
+            while (isOpen)
+            {
+                Console.SetCursorPosition(0, 20);
+                for (int i = 0; i < sectors.Length; i++)
+                {
+                    Console.WriteLine($"В секторе {i + 1} свободно {sectors[i]} мест.");
+                }
+
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine("Регестрация рейса");
+                Console.WriteLine("\n\n1 - забронировать места \n\n2 - выход из программы.\n\n");
+                Console.Write("Введите номер команды: ");
+
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        int userSector, userPlaceAmount;
+                        Console.Write("В каком секторе вы хотите летить? ");
+                        userSector = Convert.ToInt32(Console.ReadLine()) - 1;
+                        if (sectors.Length <= userSector || userSector < 0)
+                        {
+                            Console.WriteLine("Такого сектора не существует.");
+                            break;
+                        }
+                        Console.Write("Сколько мест вы хотите забронировать? ");
+                        userPlaceAmount = Convert.ToInt32(Console.ReadLine());
+                        if (userPlaceAmount < 0)
+                        {
+                            Console.WriteLine("Неверное количество мест.");
+                            break;
+                        }
+                        if (sectors[userSector] < userPlaceAmount || userPlaceAmount < 0)
+                        {
+                            Console.WriteLine($"В секторе {userSector} недостаточно места. " +
+                                $"Отстаток {sectors[userSector]}");
+                            break;
+                        }
+
+                        sectors[userSector] -= userPlaceAmount;
+                        Console.WriteLine("Бронирование успешно!");
+                        break;
+                    case 2:
+                        isOpen = false;
+                        break;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
